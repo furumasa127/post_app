16373facd31c301e91429d0332f3c4078a370a1c
EC2 Default User <149689018+furumasa127@users.noreply.github.com>
2024-04-29 08:46:57 +0000

    削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:new, :index, :edit, :show, :create, :update]
+  resources :lists, only: [:new, :index, :edit, :show, :create, :update , :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/' => 'homes#top'
 end
